737ba0fe6eec96497d224b8d2fe8b250e87e957f
atomicul <soaremihai2006@gmail.com>
2026-05-27 15:09:08 +0300

    Fix scanf specifier being incorrect on Linux in read_buffer.c
    %lld assumes int64_t is long long, which is true on macOS but not on
    Linux x86_64 where int64_t is long. Use the portable SCNd64 macro from
    <inttypes.h>, mirroring the printf fix in 685c9d5.

--- src/read_buffer.c
+++ src/read_buffer.c
@@ -1,10 +1,11 @@
 #include "bufftools.h"
 #include <stdio.h>
+#include <inttypes.h>
 
 size_t read_buffer(int64_t *nums, size_t max_count) {
     size_t size = 0;
     for(; size < max_count; size++) {
-        if (scanf("%lld", nums+size) != 1) {
+        if (scanf("%" SCNd64, nums+size) != 1) {
             break;
         }
     }
